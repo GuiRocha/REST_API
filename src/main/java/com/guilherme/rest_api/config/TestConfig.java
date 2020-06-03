c11a20055e46951b02f924f3f1299931f944ed81
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-03 17:10:41 -0300

    Refactoring TestConfig

--- src/main/java/com/guilherme/rest_api/config/TestConfig.java
+++ src/main/java/com/guilherme/rest_api/config/TestConfig.java
@@ -35,22 +35,37 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = new User(null, "Bobby Brown", "boby@gmail.com", "1234321", "12345323");
-        User user2 = new User(null, "Billy Brown", "nilly@gmail.com", "1234321", "3232345");
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
-        Order order = new Order(null, Instant.parse("2019-06-29T19:53:07Z"), user1, OrdersStatus.PAID);
-        Order order1 = new Order(null, Instant.parse("2019-06-29T19:53:07Z"), user2, OrdersStatus.WAITING_PAYMENT);
-        Category category = new Category(null, "gui");
-        Product product = new Product(null, "potato", "fresh fries", 12.90, "img");
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-        productRepository.saveAll(Arrays.asList(product));
-        categoryRepository.saveAll(Arrays.asList(category));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-        product.getCategories().add(category);
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
 
-        productRepository.saveAll(Arrays.asList(product));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-        userRepository.saveAll(Arrays.asList(user1,user2));
-        orderRepository.saveAll(Arrays.asList(order, order1));
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrdersStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrdersStatus.WAITING_PAYMENT);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrdersStatus.WAITING_PAYMENT);
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        orderRepository.save(o1);
     }
 }
